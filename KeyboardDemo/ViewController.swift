38571063e582b6092660b4ba625dc84cb4947fb6
Ricardo Bravo <ricardobravo@outlook.com>
2016-06-11 17:26:07 -0500

    chore(init): initial commit

--- KeyboardDemo/ViewController.swift
+++ KeyboardDemo/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txt: UITextField!
+    @IBOutlet weak var lbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        txt.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func show(sender: AnyObject) {
+        showMessage()
+    }
+    
+    func showMessage(){
+        self.lbl.text = txt.text
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        switch textField {
+            case txt:
+                showMessage()
+                break
+            default: break
+            
+        }
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
 
 }
 
